be23a15c3687a727bdf8c2ca14bb45cbaad7a0f0
Your Name <roberttoribio04@gmail.com>
2025-12-15 15:41:33 -0800

    adds admin.html

---
+++ backend/data.py
@@ -0,0 +1,3 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy import Column, Integer,String,Boolean,select
